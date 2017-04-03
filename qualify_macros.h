733b376c6ff2b5b3e5cd9b460a6ae6d8a3478f12
AnthonyMikh <hedgehog1024@yandex.ru>
2017-04-03 12:55:58 +0300

    Add files via upload

---
+++ qualify_macros.h
@@ -0,0 +1,15 @@
+#ifndef SGDC_PREFIX
+#define SGDC_PREFIX SGDC_
+#endif
+
+#define SGDC_Q(f) QUALIFY_WITH_T(f,T)
+#define QUALIFY_WITH_T (f,T) QUALIFY_WITH_T_(f,T)
+#define QUALIFY_WITH_T_(f,T) f ## _ ## T
+
+#define SGDC_QP(f) SGDC_QP_(SGDC_PREFIX, f, T)
+#define SGDC_QP_(p, f, T) QUALIFY_WITH_PREFIX_T (p, f, T)
+#define QUALIFY_WITH_PREFIX_T(p, f, T) p ## f ## _ ## T
+
+#define _SGDC_QP(f) _SGDC_QP_(SGDC_PREFIX, f, T)
+#define _SGDC_QP_(p, f, T) _QUALIFY_WITH_PREFIX_T (p, f, T)
+#define _QUALIFY_WITH_PREFIX_T(p, f, T) _ ## p ## f ## _ ## T
