74ab8ad3ebbc2178284487afda46cadf6aaddaf2
AnthonyMikh <hedgehog1024@yandex.ru>
2017-04-24 19:31:48 +0300

    Added to template protection from multiple SGDC_SIMPLE_DATATYPEs

--- template/template.c
+++ template/template.c
@@ -1,13 +1,16 @@
+#ifndef SGDC_SIMPLE_DATATYPE
+	#define data_node_t SGDC_Q( __name__ )
+#else
+	#ifdef _SGDC_NAME_H /* simple datatype enabled but header is included more than once*/
+	#error SGDC: cannot use SGDC_SIMPLE_DATATYPE on same data structure more than once
+#endif
+
 #ifndef _SGDC_NAME_H
 #define _SGDC_NAME_H 1
 #endif
 
 #include "../qualify_macros/qualify_macros.h"
 #include "../default_type/default_type_begin.h"
-
-#ifndef SGDC_SIMPLE_DATATYPE
-#define data_node_t SGDC_Q( __name__ )
-#endif
 
 #ifndef _MALLOC_H
 #include <malloc.h>
