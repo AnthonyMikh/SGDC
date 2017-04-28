38705d8fe72a5bc7848d8f103ad0f79ff30446d6
AnthonyMikh <hedgehog1024@yandex.ru>
2017-04-28 20:11:16 +0300

    Fixed error with unclosed #else branch

--- template/template.c
+++ template/template.c
@@ -3,6 +3,7 @@
 #else
 	#ifdef _SGDC_NAME_H /* simple datatype enabled but header is included more than once*/
 	#error SGDC: cannot use SGDC_SIMPLE_DATATYPE on same data structure more than once
+	#endif
 #endif
 
 #ifndef _SGDC_NAME_H
