37f77f83306231bfe327c063709feb7890a8d2f0
AnthonyMikh <hedgehog1024@yandex.ru>
2017-05-09 22:22:08 +0300

    Create bintree_iter_types.h
    This file contains definition of binary tree iterator internal state and definition of binary tree iterator type (right to left and left to right).

---
+++ bintree/bintree_iter_types.h
@@ -0,0 +1,16 @@
+#ifndef _SGDC_BINTREE_ITER_TYPES_H
+#define _SGDC_BINTREE_ITER_TYPES_H 1
+
+typedef enum {
+	  _SGDC_bintree_to_left
+	, _SGDC_bintree_to_right
+	, _SGDC_bintree_from_left
+	, _SGDC_bintree_from_right
+} SGDC_bintree_iter_state;
+
+typedef enum {
+	_SGDC_bintree_left_to_right,
+	_SGDC_bintree_right_to_left
+} SGDC_bintree_iter_type;
+
+#endif /* _SGDC_BINTREE_ITER_TYPES_H */
