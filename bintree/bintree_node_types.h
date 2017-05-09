86e9f3b3da9ac404c66c5b53f1658de55c2e26ea
AnthonyMikh <hedgehog1024@yandex.ru>
2017-05-09 23:07:46 +0300

    Create bintree_node_types.h
    This file contains definition of bintree node types

---
+++ bintree/bintree_node_types.h
@@ -0,0 +1,10 @@
+#ifndef _SGDC_BINTREE_NODE_TYPES_H
+#define _SGDC_BINTREE_NODE_TYPES_H 1
+
+typedef enum {
+	  _SGDC_bintree_node_root
+	, _SGDC_bintree_node_left
+	, _SGDC_bintree_node_right
+} _SGDC_bintree_node_type;
+
+#endif /* _SGDC_BINTREE_NODE_TYPES_H */
